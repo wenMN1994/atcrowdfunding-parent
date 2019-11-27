6eeb9753f94c0c842f667ee704222272d0608278
温文星 <18475536452@163.com>
2019-11-27 22:11:05 +0800

    日志框架整合测试

--- atcrowdfunding-web/src/test/java/com/dragon/test/Slf4jTest.java
+++ atcrowdfunding-web/src/test/java/com/dragon/test/Slf4jTest.java
@@ -1,5 +1,8 @@
 package com.dragon.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
@@ -10,12 +13,12 @@ package com.dragon.test;
  */
 public class Slf4jTest {
     public static void main(String[] args) {
-        /*Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
+        Logger logger = LoggerFactory.getLogger(Slf4jTest.class);
         logger.debug("debug..."); //用于调试程序
         logger.info("info...");//用于请求处理提示消息
         logger.warn("warn...");//用于警告处理提示消息
         logger.error("error...");//用于异常处理提示消息
-        logger.error("==>>"+logger.getClass());*/
+        logger.error("==>>"+logger.getClass());
 
     }
 }
